0642c0e1ad89a8ba3c740ce073e61510a8a71585
Ilya <iliakrapivkin@gmail.com>
2020-11-24 21:39:15 +0300

    options_created

---
+++ delete1.js
@@ -0,0 +1,19 @@
+{
+  account: {
+    token: ...,
+    name: ...,
+    keywords: ...
+  },
+  marker: {
+    enabled: ...,
+    stats: ...
+  },
+  settings: {
+    matchWhole: ...,
+    matchCase: ...,
+    color: ...,
+    colorBg: ...,
+    bold: ...,
+    underline: ...
+  }
+}
--- src/options.js
+++ src/options.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './views/Options/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import storeCreatorFactory from 'reduxed-chrome-storage';
+import reducers from './reducers';
+import 'semantic-ui-css/semantic.min.css';
+import App from './views/Options/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); 
+(async () => {
+  const store = await storeCreatorFactory({createStore})(reducers);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+})();
